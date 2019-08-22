8229f647318d90b970f1e1e16cec5cef0e5dc7c2
unknown <riskerkk@igsinc.co.kr>
2019-08-22 14:59:27 +0900

    update

--- zoopeter_log/src/main/java/zoopeter/log/controller/PostbackAppsflyerController.java
+++ zoopeter_log/src/main/java/zoopeter/log/controller/PostbackAppsflyerController.java
@@ -29,7 +29,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+ 
 @Controller
 @RequestMapping("/appsflyer")
 public class PostbackAppsflyerController {
